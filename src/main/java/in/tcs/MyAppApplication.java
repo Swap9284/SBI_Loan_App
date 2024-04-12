3ae3ca3114312f60f7f4abc8137c5ce16fb6d0d5
Swap9284 <swapnilsahdevm@gmail.com>
2024-04-12 23:35:58 +0530

    line change

--- src/main/java/in/tcs/MyAppApplication.java
+++ src/main/java/in/tcs/MyAppApplication.java
@@ -7,8 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MyAppApplication {
 
 	public static void main(String[] args) {
-
-		int a=10;
+        int a=10;
 		SpringApplication.run(MyAppApplication.class, args);
 	}
 
